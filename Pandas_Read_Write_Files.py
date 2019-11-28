ee81d399705c3e36ac85ac1fec0625d35a2af550
Allif_Izzuddin <allif_izzuddin@pop-os.localdomain>
2019-11-28 23:59:10 +0800

    create new csv

--- Pandas_Read_Write_Files.py
+++ Pandas_Read_Write_Files.py
@@ -8,3 +8,20 @@ print(df,'\n')
 
 df =pd.read_csv("stock_data.csv", nrows=3) #only read 3 rows
 print(df,'\n') 
+
+#cleanup messy data, n.a, not available to NaN
+df = pd.read_csv("stock_data.csv", na_values=["not available","n.a."])
+print(df,'\n')
+
+#use dict to cleanup messy data (data munging/ data wrangling)
+#revenue never -1, so convert it to NaN
+
+df = pd.read_csv("stock_data.csv", na_values={'eps': ["not available", "n.a."],
+'revenue': ["not available", "n.a.", -1],
+'people': ["not available", "n.a."], 'price':["not available", "n.a."]
+})
+print(df,'\n')
+
+#WRITING BACK TO CSV
+
+df.to_csv("new.csv", index=False) #with remove index column
