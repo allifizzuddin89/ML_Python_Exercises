ad5a114239bc648fd4b7d4a85ad7fee387733f4e
Allif_Izzuddin <allif_izzuddin@pop-os.localdomain>
2019-11-28 22:55:49 +0800

    upload stock data1

--- Pandas_Read_Write_Files.py
+++ Pandas_Read_Write_Files.py
@@ -1,1 +1,10 @@
-import pandas as import pd
+import pandas as pd 
+df = pd.read_csv("stock_data.csv", skiprows=1) #skip row 1 from top, also similar to header=1
+print(df,'\n')
+
+#stock_data1.csv has no header
+df = pd.read_csv("stock_data1.csv", header=None, names=["TICKER", "EPS", "REVENUE", "PRICE", "PEOPLE"]) #if header is missing, we can put it manually
+print(df,'\n')
+
+df =pd.read_csv("stock_data.csv", nrows=3) #only read 3 rows
+print(df,'\n') 
