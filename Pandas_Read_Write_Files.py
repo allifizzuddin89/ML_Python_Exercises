4013835f588568c5c2da00a1b79f0ee76fdb25d4
Allif_Izzuddin <allif_izzuddin@pop-os.localdomain>
2019-12-03 01:23:17 +0800

    2 df into 1 xlsx

--- Pandas_Read_Write_Files.py
+++ Pandas_Read_Write_Files.py
@@ -76,5 +76,23 @@ print(df)
 df.to_excel("new.xlsx", sheet_name="stock", startrow=6, startcol=2) #start writing at certain position in excel cell
 
 #WRITE 2 DATAFRAME INTO ONE SAME EXCEL FILE
+#using excel writer
 
+df_stocks = pd.DataFrame({
+    'tickers': ['GOOGL', 'WMT', 'MSFT'],
+    'price': [845, 65, 64 ],
+    'pe': [30.37, 14.26, 30.97],
+    'eps': [27.82, 4.61, 2.12]
+})
+
+df_weather =  pd.DataFrame({
+    'day': ['1/1/2017','1/2/2017','1/3/2017'],
+    'temperature': [32,35,28],
+    'event': ['Rain', 'Sunny', 'Snow']
+})
+
+with pd.ExcelWriter('stock_weather.xlsx') as writer:
+    df_stocks.to_excel(writer, sheet_name="stocks")
+    df_weather.to_excel(writer, sheet_name="weather")
+    
 
