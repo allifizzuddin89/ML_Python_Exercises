d8ab5bd175a8f0b79c35ce3ac97eba2c34e3eba3
Allif_Izzuddin <allif_izzuddin@pop-os.localdomain>
2020-01-20 16:22:28 +0800

    numpy import data & advance masking and indexing

--- numpy_importdata.py
+++ numpy_importdata.py
@@ -14,3 +14,23 @@ print('\n',data.dtype)
 data = data.astype('int32')
 print('\n',data)
 print('\n',data.dtype)
+
+#Boolean Masking and Advanced Indexing
+
+#advance slicing, find element greater than 40
+#masking and condition
+print('\n',data>40)
+print('\n',data[data>40])
+
+print('\n',np.any(data>40, axis=0)) #downward matrix
+print('\n',np.any(data>40, axis=1)) #rightward compare matrix
+print('\n',"higher than 40 and less than 60",'\n',((data>40)&(data<60)))
+print('\n',"the elements are: ",'\n',data[(data>40)&(data<60)])
+
+
+#index with a list in numpy
+#rearrange index
+print('\n',data[[2,1,0]])
+#compare with original data
+print('\n',data)
+
