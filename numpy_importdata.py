8c052008606db26d211d003e86416c9b21c50013
Allif_Izzuddin <allif_izzuddin@pop-os.localdomain>
2020-01-20 16:00:01 +0800

    numpy import data execise

--- numpy_importdata.py
+++ numpy_importdata.py
@@ -1,3 +1,16 @@
 #Written by Allif Izzuddin bin Abdullah
 #github : allifizzuddin89
 #Numpy import data exercise
+
+import numpy as np 
+
+data = np.genfromtxt('numpy_data.txt', delimiter=',')
+print('\n',data)
+print('\n',data.dtype)
+
+#we notice the data type is float
+#change into integer
+
+data = data.astype('int32')
+print('\n',data)
+print('\n',data.dtype)
